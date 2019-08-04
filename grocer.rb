a2d7d20a97d0ce93e4b74cef6e5c227c868da4a1
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 05:18:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,13 @@
 
 #accepts the array and turns it into a hash. the hash will include item counts
 
-items.each do |item|
-  item.each do |key, val|
-    puts key
-    puts val
-  end
-end
-
 def consolidate_cart(cart)
   cart.each do |item|
     item.each do |item_name, item_properties|
-    puts item_name
-    puts item_properties 
+      puts item_name
+      puts item_properties 
     end 
+  end 
 end
 
 
